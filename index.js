a77d10969ec42a5e114c376f4307a53b63a054a9
sujonifty <sujonifty@gmail.com>
2024-05-10 20:27:57 +0600

    completed: cardDetails  section

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const port = process.env.PORT || 5000;
 // middleware 
 app.use(
     cors({
-      origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-      ],
-      credentials: true,
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+        ],
+        credentials: true,
     })
-  );
+);
 app.use(express.json());
 
 
@@ -24,18 +24,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
+    try {
 
 
-    const assignmentCollection = client.db('assignmentDB').collection('assignments');
+        const assignmentCollection = client.db('assignmentDB').collection('assignments');
         app.post('/addAssignment', async (req, res) => {
             const assignment = req.body;
             console.log(assignment);
@@ -57,27 +57,34 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/cardDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await assignmentCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.delete('/assignments/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await assignmentCollection.deleteOne(query);
             res.send(result);
         })
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('server is running');
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('server is running on port:', port);
 })
 
